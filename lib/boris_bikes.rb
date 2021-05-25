937cf972ae3a139829e6309e728f698b3a8b589a
OptimusWall-E <iemailedmichael@gmail.com>
2021-05-25 18:59:59 +0100

    added return_bike method to Bike class, boris_bikes.rb

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -2,11 +2,18 @@ class DockingStation
   def release_bike
     bike = Bike.new
   end
+
 end
+
+@docked_bikes = 10
 
 class Bike
   def working?
-  rand(0..1) == 1 ? true : false
+    rand(0..1) == 1 ? true : false
+  end
+
+  def return_bike(docked)
+    @docked_bikes += 1
   end
 end
 
