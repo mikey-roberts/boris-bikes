355d7af5506f93082aa1560dfdfee75b6a0ef712
mikey-roberts <mikey_roberts@outlook.com>
2021-05-26 19:00:19 +0100

    release bike if capacity == 0

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,11 +1,16 @@
 class DockingStation
   attr_reader :dock, :bike
+  attr_accessor :capacity
+  
+  @capacity = 10
 
   def dock(bike)
     @bike = bike
   end
 
   def release_bike
+  nil if @capacity == 0
+    @capacity -= 1 
     bike = Bike.new
   end
 
