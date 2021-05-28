4b11d052f6e16dd2b14b83da5a13d21239384cc1
mikey-roberts <mikey_roberts@outlook.com>
2021-05-28 10:14:24 +0100

    Chapter 15 refactored full and empty into private methods

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -5,13 +5,23 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bikes)
-    fail 'Dock is full' if @bikes.count >= 20
+    fail 'Dock is full' if full?
     @bikes << bikes
+  end
+
+private
+
+  def full?
+    @bikes.count >= 20   
+  end
+
+  def empty?
+    @bikes.empty?
   end
 end
 
